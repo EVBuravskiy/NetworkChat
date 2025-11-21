417edb3f7490e9da3395610b49c02df2cc2d4d2e
EBuravskiy <elric0sphinx@gmail.com>
2025-11-21 16:06:46 +0500

    Application host created

---
+++ NetworkHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace NetworkHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost Host = new ServiceHost(typeof(NetworkChat.ServiceChat)))
+            {
+                Host.Open();
+                Console.WriteLine("Хост приложения запущен!");
+                Console.WriteLine("Для остановки хоста нажмите любую клавишу");
+                Console.ReadLine();
+            }
+        }
+    }
+}
